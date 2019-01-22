067d2174e6cb7cfcb76e1ec0944892650134a818
Tom Barnes <barnes.thomas.c@gmail.com>
2019-01-22 12:45:40 -0500

    updated media queries

--- src/detour.js
+++ src/detour.js
@@ -1,12 +1,16 @@
 // SIZING THE DETOUR VIEW HIEGHT FOR THE WINDOW
 
 let vh = window.innerHeight * 0.01;
+let vw = window.innerWidth * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vw', `${vw}px`);
 
 window.addEventListener('resize', () => {
     // Same script as before on resize
     let vh = window.innerHeight * 0.01;
+    let vw = window.innerWidth * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
 });
 
 
