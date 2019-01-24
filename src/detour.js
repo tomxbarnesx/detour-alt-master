fbb367e03f188b3f5daea9aadc946a7668fa1d7d
Tom Barnes <barnes.thomas.c@gmail.com>
2019-01-23 20:10:38 -0500

    updated button styling and glow effect

--- src/detour.js
+++ src/detour.js
@@ -20,7 +20,11 @@ let detourButtons = document.getElementsByClassName("detour-button");
 let detourVids = document.getElementsByClassName("detour-vid");
 let detourReturn = document.getElementById("detour-return");
 let detourRestart = document.getElementById("detour-restart");
+let returnRing = document.getElementsByClassName("return-ring")[0];
+let ring = document.getElementById("ring");
 let active;
+
+// ADD EVENT LISTENERS FOR THE DETOUR TRIGGER BUTTONS AND EFFECTS TO THE RETURN BUTTON ON VIDEO END
 
 for (let i = 0; i < detourTriggers.length; i++) {
     detourButtons[i].id = "trigger"+i;
@@ -30,17 +34,27 @@ for (let i = 0; i < detourTriggers.length; i++) {
         detourVids[active].style.display = "block";
         detourVids[active].play();
     });
-    // detourVids[i].addEventListener("ended", event => {
-    //     detourReturn.classList.add("glow");
-    // })
+    detourVids[i].addEventListener("ended", event => {
+        returnRing.id = "ring"
+    })
 };
+
+// ADD EVENT LISTENER TO THE RETURN SCROLL
 
 detourReturn.addEventListener("click", function(){
     document.getElementById('return-target').scrollIntoView({behavior:"smooth"});
     detourVids[active].pause();
     detourVids[active].style.display = "none";
+    if (returnRing.id == "ring") {
+        returnRing.removeAttribute("id");
+    }
 })
+
+// RESTART BUTTON EVENT LISTENER
 
 detourRestart.addEventListener("click", function() {
     detourVids[active].currentTime = 0;
+    if (returnRing.id == "ring") {
+        returnRing.removeAttribute("id");
+    }
 })
